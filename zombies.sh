e784246891c4370b6d16bb3d2ea1fba25c6bcfbe
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2010-09-11 20:58:00 +1200

    Copy post over from flaviusb.net.
    This was way more an unfinished scribble than I remember.

---
+++ zombies.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export TITLE="Zombies"
+export TAGS="philosophy scribble"
+export CANONICAL="zombies"
+export TEXT=$(markdown -f nopants -f footnote << END
+
+The Zombie World hypothesis attempts to illustrate the conflict between our intuitive understanding of our experience of mind (or more specifically, qualia) with our understanding of the character of physical law. That is to say, we have brains, which should be able to be modelled by physical laws; these physical laws do not seem to necessarily give rise to the ability to perceive qualia.
+
+ and where damage to them results in damage to the mind...
+
+So, if the zombie world hypothesis is correct (that is, it is *logically possible* for a zombie to exist)... chatbots... symbol manipulation games...
+
+If the zombie world hypothesis is incorrect, then any sufficiently good model of a sentient being must itself be sentient.
+
+That is to say, the bridging laws must be capturable within the character of physical law, so if physical law can be modelled, then our models themselves can...
+
+This has horrifying consequences; every simulation which meets those criteria...
+
+Epiphenomenalism vs direct religious revelation; both require privileged epistemic access to verify; inside vs outside display. Identical displays could be given by people who have or have not had either. _Unless_ one makes the posit that the same external behavior must result from the same internal state.
+
+This also depends on whether mental objects exist...
+That is, whether the material world is privileged in some magical way; if you are a reductionist materialist then ...
+
+END
+)
