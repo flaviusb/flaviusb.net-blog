7bb2e587e02f93f54ef993186e2ce72363303c94
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2010-12-31 20:03:00 +1300

    Copy post over from flaviusb.net.

---
+++ implode.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export TITLE="Implode(:flaviusb)"
+export TAGS="programming short"
+export CANONICAL="implode"
+export TEXT=$(markdown -f nopants -f footnote << END
+So, I have finally got on the 2010 bandwagon, by imploding my web presence (somewhat). My tweets are now on flaviusb.net/tweets/..., I have a my own shorturl service at flaviusb.me. I'll write an actual post about this after the New Years party I'm late to. Sigh.
+END
+)
