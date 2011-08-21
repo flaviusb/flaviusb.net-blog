1441b103826a2993317d9a47af700e0eecf12b41
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2011-08-21 13:42:00 +1200

    Update post to version from flaviusb.net.

--- implode.sh
+++ implode.sh
@@ -3,6 +3,18 @@ export TITLE="Implode(:flaviusb)"
 export TAGS="programming short"
 export CANONICAL="implode"
 export TEXT=$(markdown -f nopants -f footnote << END
-So, I have finally got on the 2010 bandwagon, by imploding my web presence (somewhat). My tweets are now on flaviusb.net/tweets/..., I have a my own shorturl service at flaviusb.me. I'll write an actual post about this after the New Years party I'm late to. Sigh.
+So, I have finally got on the 2010 bandwagon, by imploding my web presence (somewhat). My tweets, blog, and some of my code and docs are on this site, and I have my own shorturl service. For example, [http://flaviusb.me/t/45M/0](http://flaviusb.me/t/45M/0) is a shortlink to a tweet about my general programming life; sadly, the tweet is still relevant.
+ 
+
+
+But, what is implosion, and why do it? What forces were pushing in this direction?
+
+Well, most digital identities these days are smeared across a myriad of sites, with chunks siloed away from each other and under the control of corporations, who may or may not continue to provide services in the same manner they have in the past. The initial prod for me was a dissatisfaction with blogger as a blogging platform;
+
+Another important reason was for the persistence of memory... anecdotes...
+For example, going back to [this tweet](http://flaviusb.me/t/47u/1) also takes me back to the mental state I was in when I wrote it...
+I am a bit of a packrat, especially as far as journals and notebooks go. I periodically reread my old journals; this allows me to more accurately track how I have changed as a person over the years; I find that I also gain new perspectives on... sometimes I have an idea which even I am not yet ready for, but when I reread it years later I...
+
+The site is currently a mess; Jekyll here, node.js and Jade there, pulled together with shell scripts and number eight wire.
 END
 )
