4d48e27b1a3a25714d231fb3d64d8b1a8e3f31f0
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2012-01-31 03:10:00 +1300

    Add disentangling autoantonyms from flaviusb.net

---
+++ disentangling-autoantonyms.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+export TITLE="Disentangling some annoying autoantonyms; or, politics, the mind killer"
+export TAGS="politics language pedantry rant"
+export CANONICAL="disentangling-autoantonyms"
+export TEXT=$(markdown -f nopants -f footnote << END
+This post was prompted by repeated headdesking caused by running into the same word being used in rather different ways by people in political arguments, and having people respond explosively when I try to pin them down on a meaning or discuss why what I am talking about is different. Now, I do not live in the US, so at least most people here can have 'Communism' and 'Socialism' explained to them and not explode before you have finished the first sentence, but I do still run into some problems with that. Trying to explain what I mean or pin down what other people mean by 'Capitalism' is worse though. So, first an overall explanation, then a simple example, then more of an explanation, then the Big Three.
+
+Why Autoantonyms?
+-----------------
+
+In English, as in many languages, words can have several distinct, even contradictory meanings. A word that in some sense is its own opposite is an autoantonym; an example pointed out by [supergee](http://supergee.livejournal.com/2645438.html) is 'sanction', which can mean either explicit endorsement or forceful denial. Politics complicates this, with words being deliberately coopted, smeared, or set up as strawmwen, both with and without conscious intention. This promotes equivocation in arguments, which is really quite problematic; even worse, it tends to promote a kind of unconscious equivocation, and the things that give rise to this also make even trying to bring this to light in order to be able to actually have a conversation quite difficult.
+
+### A concrete example
+
+We will pick something easy, where there is a single common strawman meaning which is straightforwardly used to oppose the single common actual political science meaning for the word; the word is Anarchism.
+
+First, the strawman meaning: violent lawlessness, with men in black masks smashing, burning and murdering with impunity.
+
+Then, the political science meaning, which Anarchists mean when they refer to Anarchism: not needing leaders or coercive social relations; a form of governance comprised of direct action and mutual aid, with the potential addition of direct democracy, personal property (as distinct from private property) and workers collectives amongst other things; a system which completely rejects private property, the state, corporations, wage slavery and potentially various other things.
+
+### How equivocation is a problem
+
+If you know what equivocation is, and you can already see the problem, you can skip this subsection. Otherwise,  what remains is to demonstrate what equivocation has to do with all of this. The answer is that in a political debate, an Anarchist will be explicitly advocating a political system based on the 'political science' meaning above, but will commonly receive a brushoff argument based on the 'strawman' meaning, like so:
+
+"So you advocate Anarchism, eh? Well, that is bad, and also how would we have roads and food if people just wear black masks and smash things? Therefore you are wrong, and Anarchism is bad."
+
+If we unpack that argument that purports to be a syllogism however, we get this argument:
+
+"So you advocate 'violent lawlessness, with men in black masks smashing, burning and murdering with impunity', eh? Well, that is bad, and also how would we have roads and food if people just wear black masks and smash things? Therefore you are wrong, and 'not needing leaders or coercive social relations; a form of governance comprised of direct action and mutual aid, with the potential addition of direct democracy, personal property (as distinct from private property) and workers collectives amongst other things; a system which completely rejects private property, the state, corporations, wage slavery and potentially various other things' is bad."
+
+Boom, equivocation; the use of Anarchism in one part of the argument is different from the other, and for the argument to be valid the two uses have to be the same. The speaker explicitly rejects the strawman version of the thing (here Anarchism) with a reasonable argument, and then implicitly equivocates over the meaning of the word used to identify the thing in question to allow them to reject the actual version as well. One would think that stopping this would be as simple as saying:
+
+"Hang on now, Anarchism is a word that has several meanings, and the meaning that I refer to in my advocacy is the political science meaning, which is different from the meaning you are arguing against here, like so..."
+
+Unfortunately, it isn't; hence, this long prelude to the real rant.
+
+### Insanely held beliefs
+
+These beliefs in signs signifying only either strawman arguments or 'the obviously correct thing' do not seem to be merely strongly held; they seem to be held in defiance of all proof, argument or opposition. This seems to me to be largely because of tribal identification, ingroup-outgroup behaviour, and ultimately fear of death; people end up with powerful emotions, and even their identity invested in these definitions. Tribal identification over time turns shibboleths into actual beliefs, and ingroup-outgroup effects lead to investment of identity in dehumanization and demonization of the outgroup, and investment of identity in the rightness of the ingroup. This explains why people react (in the case of mere strong emotions) with seemingly random emotional outbursts, or (in the case of identity) as if it will kill them if they understand or even acknowledge your words, leading to responses ranging from trite thoughtless (and yet final) brushoffs through to paroxysms of rage.
+
+So, Anarchism was comparatively easy to talk about because few people have their identity invested in the idea that the strawman version of Anarchism is the true one, but there often is still some difficulty, caused by spillover from people having identity tied up in some other system (such as Capitalism) being The Only Right Answer, and therefore some identity tied up in there not being any reasonable alternatives.
+
+### The Big Three defined
+
+Now on to something harder; a rant in bullet point form, if you will. I present a short, incomplete and contradictory list of the things that people often refer to using the words Capitalism, Communism or Socialism below. My comments occur in brackets. If you ragequit when reading these lists, you are demonstrating the point of the rant.
+
+#### Capitalism
+- as vs Mercantilism (an actual historical perspective about the economic and political system as was relevant in the 18th century in Western Europe; discussions about balance of trade vs comparative advantage etc)
+- as vs Feudalism (a fantasy not grounded in history; Feudalism gave way to eg Mercantilism before the invention of Capitalism, even in hereditary despotic states)
+- a naive 'Market-ism' or 'Trade-ism' (useless for argument, as it is vitiated of all that distinguishes it from eg Mercantilism, many forms of Anarchism and Socialism, traditional Maori lifeways...)
+- a system of creative destruction
+- a way of organising independent agents without collusion: money as a sensor, the utility function reduced to currency, avoiding information problems that cause problems in centralised economies
+- as a shorthand for some kind of 'corporatism' - usually not related to the political science definition of Corporatism
+- 'Gordon Gekko'; the idea that 'greed is good' or that 'I was just maximizing shareholder value' is a good way of living
+- "Undead capital feeding on the lifeblood of living labour" (Marx's analysis was of the ideal of the Capitalists, not necessarily existing Capitalism, so it requires some justification to use this in an argument)
+- a system of rentiers, wage slavery, interest, dividends, capital as private property, and financial instruments used to 'create' a profit
+- the only way that works (that is a question of fact which can be tested, and if left as a bare assertion is rather begging the question)
+- 'Right Libertarianism'
+- the 'Washington Consensus'
+- the Free World (more a label than anything else. Much like First World vs Second World)
+- death camps (see Naiomi Klein's fantastic book 'The Shock Doctrine' for a short and sweet intro to modern capitalism and death camps)
+
+
+#### Communism
+- death camps (see 'Stalinism', the Khmer Rouge or North Korea for an intro to this)
+- 'Socialism'
+- 'Stalinism' (That is, Stalin was a Communist, so all Communists must be advocating Stalinism)
+- 'Liberalism' or 'Leftism' (This seems to be more of a bizarre form of slur than any actual reasoned belief)
+- "From each according to his abilities to each according to his needs"
+- 'Marxism'
+- 'Market socialism' a la China
+- a classless, moneyless, stateless, socialist society with common ownership of the means of production
+- 'council communism'
+- 'non-Marxist libertarian communism'
+- 'anarcho-communism'
+
+
+#### Socialism
+- death camps (see 'Communism' below, and then see 'Stalinism', the Khmer Rouge or North Korea for an intro to this)
+- 'Communism' (Yes, some people think that Socialism is just a code word for Communism)
+- "an economic system characterized by social ownership or control of the means of production and cooperative management of the economy"
+- Redistributive taxation, social welfare programs, and government regulation (As occurs through the Scandinavian countries, and in various other Western countries already)
+- 'Marxist-Lenninism'
+- 'Libertarianism'
+- 'Libertarian socialism'
+- Various forms of Anarchism
+- 'Mutualism'
+- 'Social Democracy'
+- Any movement concerned with the social problems caused by the adoption of Capitalism in the 18th century.
+
+
+Now, it shouldn't need to be said, but this is merely a list of things that I have encountered other people claiming were the true and only meanings for the words listed; in an argument, I am much more interested in agreeing to a common vocabulary where all parties know what meaning is meant by all relevant words, and if necessary tabooing words that are contentious or making up alternative words which can have a well defined meaning, than I am in claiming specific words for myself. For example, when arguing about the place of Capitalism in the world, I like to split 'Capitalism' apart into different made up words that are not a confused jumble, such as market-ism, money-ism, bank-ism, rent-ism, corporation-ism. Whereas in a discussion of 'Capitalism' vs 'Communism' in terms of ideal ideology to rule the world, I would first reject that false dichotomy, and then want to split Capitalism up into at least vampire-capital-rent-etc-ism, creative-destruction-ism, money-ism, and distributed-decision-making-ism, such that these different things could actually be talked about as different things.
+
+### PS: My sinister motivation
+
+While I do not have any of my identity tied up in any of these particular words, I do have a fairly large part of my identity tied up in being able to talk, discuss and argue meaningfully with other people. So, my sinister motivation for actually writing this up is that now when I have these arguments online, I can point the other parties at this url when these problems occur, and hopefully avoid some of the rage and terror inducing sensation of having people respond to discussion with equivocation, explosions, and exit.
+
+END
+)
